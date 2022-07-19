a916f2baed31b6a707bb491d8c70466e83e4a72b
Yudi Alviska <yudi.a@weefer.co.id>
2022-07-19 17:21:34 +0700

    edit App.js

--- App.js
+++ App.js
@@ -1,69 +1,83 @@
 import React, {useEffect} from 'react';
-import { View, StyleSheet, ActivityIndicator, Platform, Alert } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Platform,
+  Alert,
+} from 'react-native';
 import {
   PERMISSIONS,
   openSettings,
-  requestMultiple,
+  check,
+  request,
+  RESULTS,
 } from 'react-native-permissions';
-import { useCameraDevices, Camera } from 'react-native-vision-camera'
+import {useCameraDevices, Camera} from 'react-native-vision-camera';
 
 export default function App() {
-  const devices = useCameraDevices('wide-angle-camera')
-  const device =  devices.front
+  const devices = useCameraDevices('wide-angle-camera');
+  const device = devices.front;
 
   useEffect(() => {
-    let arrayCheck = [];
     if (Platform.OS === 'ios') {
-      arrayCheck = [
-        PERMISSIONS.IOS.CAMERA,
-      ];
-    } else {
-      arrayCheck = [
-        PERMISSIONS.ANDROID.CAMERA,
-      ];
-    }
-    requestMultiple(arrayCheck)
-      .then(() => {})
-      .catch(() => {
-        Alert.alert(
-          'Information',
-          'Permission is blocked, enable manually on setting',
-          [
-            {
-              text: 'Not Now',
-              onPress: () => {},
-            },
-            {
-              text: 'Goto Setting',
-              onPress: () => openSettings(),
-            },
-          ],
-          {cancelable: true},
-        );
+      check(PERMISSIONS.IOS.CAMERA).then(status => {
+        if (status === RESULTS.GRANTED) {
+        } else if (status === RESULTS.DENIED) {
+          request(PERMISSIONS.IOS.CAMERA);
+        } else {
+          _showAlertPermission();
+        }
       });
-  }, [])
+    } else if (Platform.OS === 'android') {
+      check(PERMISSIONS.ANDROID.CAMERA).then(status => {
+        if (status === RESULTS.GRANTED) {
+        } else if (status === RESULTS.DENIED) {
+          request(PERMISSIONS.ANDROID.CAMERA);
+        } else {
+          _showAlertPermission();
+        }
+      });
+    }
+  }, []);
+
+  function _showAlertPermission() {
+    Alert.alert(
+      'Information',
+      'Permission is blocked, enable manually on setting',
+      [
+        {
+          text: 'Not Now',
+          onPress: () => {},
+        },
+        {
+          text: 'Goto Setting',
+          onPress: () => openSettings(),
+        },
+      ],
+      {cancelable: true},
+    );
+  }
 
   return (
     <View style={styles.container}>
       {device ? (
-        <Camera
-          style={styles.wrapCamera}
-          device={device}
-          isActive={true}
-        />
-      ): (<ActivityIndicator size={'large'} />)}
+        <Camera style={styles.wrapCamera} device={device} isActive={true} />
+      ) : (
+        <ActivityIndicator size={'large'} />
+      )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red'
+    backgroundColor: 'red',
   },
   wrapCamera: {
     height: 300,
     width: 300,
-    backgroundColor: 'green'
-  }
-})
+    backgroundColor: 'green',
+  },
+});
